62f2048eedd6ae93ed5df59335965e507fa4a1dc
jrode010 <jrode010@fiu.edu>
2024-08-16 11:21:55 -0400

    Additional work

--- Cleaning_code_shrimp_acoustic_data.R
+++ Cleaning_code_shrimp_acoustic_data.R
@@ -2,3 +2,25 @@
 #Authors: Jon Rodemann and Rolando Santos
 #Packages
 library(tidyverse)
+library(splitstackshape)
+library(chron)
+library()
+
+HR2_list <- list.files("./Data/", full.names = TRUE)
+files_all <- function(x){
+  df <- lapply(x, read_csv, skip = 37, col_names = FALSE,) %>% 
+    # subset(., X1 %in% c("DET"))%>%
+    # bind_rows %>% 
+    plyr::rbind.fill %>%
+    subset(., select = 1:13) %>%
+    
+    mutate(X10 = as.character(X10))  #mutate here is to force X10 as character so I can use rbind later and transfer the number/name of object within the list as a column
+  
+}
+
+HR2_all <- lapply(HR2_list, read_csv, skip = 37, col_names = FALSE) %>% 
+  # subset(., X1 %in% c("DET"))%>%
+  # bind_rows %>% 
+  list_rbind() #%>%
+  #subset(., select = 1:13)
+
