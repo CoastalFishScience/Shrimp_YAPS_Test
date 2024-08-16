b29a76eea134607f7362bba7abe7f7965e1f35e7
jrode010 <jrode010@fiu.edu>
2024-08-16 12:09:08 -0400

    Vue dataset for HR2 and new code

--- Cleaning_code_shrimp_acoustic_data.R
+++ Cleaning_code_shrimp_acoustic_data.R
@@ -5,6 +5,7 @@ library(tidyverse)
 library(splitstackshape)
 library(chron)
 library(yaps)
+library(data.table)
 
 
 HR2_list <- list.files("./Data/", full.names = TRUE)
@@ -17,9 +18,17 @@ HR2_all <- lapply(HR2_list, read_csv, skip = 37, col_names = FALSE) %>%
 
 colnames(HR2_all)<-c("Type", "Date.Time.UTC", "Date.Time.Offset", "trash",
                         "Time.Offset", "Model",
-                        "Receiver.ID", "Detection.Type", "Full.ID",
-                        "ID", "Signal.DB", "Noise.DB", 'Q.score')
+                        "Receiver.ID", "Detection.Type", "ID",
+                        "Full.ID", "Signal.DB", "Noise.DB", 'Q.score')
 
 HR2_all_det <- HR2_all %>% dplyr::filter(Type == 'DET') %>% select(-c(trash, Signal.DB, Noise.DB, Q.score)) 
 
-#
+#Change the HR2 output into vue output
+HR2 <- HR2_all_det
+
+HR2_1 <- HR2 %>% select(c(Date.Time.Offset, Model, Receiver.ID, Full.ID, Detection.Type)) %>% dplyr::filter(Detection.Type == 3) %>% 
+  mutate(Receiver = paste(Model, Receiver.ID, sep = '-')) %>% select(-c(Model, Receiver.ID, Detection.Type)) %>% rename('Date.and.Time..UTC.' = Date.Time.Offset, 'Transmitter' = Full.ID) %>% 
+  mutate(Transmitter.Name = NA, Transmitter.Serial = NA, Sensor.Value = NA, Sensor.Unit = NA, Statione.Name = NA, Latitude = NA, Longitude = NA, Transmitter.Type = NA, Sensor.Precision = NA)
+
+
+                      
